68f6f524085a50d0c15e098bbce144c8d16bd341
长生 <jacke.yang@uccvr.com>
2025-09-25 13:34:29 +0800

    添加截止日期管理器

---
+++ Unity/ReunionMovement/Assets/ReunionMovement/Utils/Deadline/Editor/DeadlineMgrEditor.cs
@@ -0,0 +1,23 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DeadlineMgr))]
+public class DeadlineMgrEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        GUILayout.Space(10);
+        if (GUILayout.Button("清除截止日期本地数据 (PlayerPrefs)", GUILayout.Height(30)))
+        {
+            if (EditorUtility.DisplayDialog("清除本地数据", "确定要清除 DeadlineMgr 的本地 PlayerPrefs 数据吗？", "确定", "取消"))
+            {
+                PlayerPrefs.DeleteKey("Deadline_LastUtcTicks_v1");
+                PlayerPrefs.DeleteKey("Deadline_LastUtcHash_v1");
+                PlayerPrefs.Save();
+                EditorUtility.DisplayDialog("完成", "已清除 DeadlineMgr 的本地数据。", "OK");
+            }
+        }
+    }
+}
